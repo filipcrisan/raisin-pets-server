fc8fe924e7a37719f5078006ebcbdacabbe3e7ef
Filip Crisan <66752246+filipcrisan@users.noreply.github.com>
2023-03-07 17:46:15 +0200

    feat: add content to tutorial

--- raisin-pets.Common/Dtos/Tutorial/TutorialDto.cs
+++ raisin-pets.Common/Dtos/Tutorial/TutorialDto.cs
@@ -10,4 +10,5 @@ public class TutorialDto
     public int MinAgeInYears { get; set; }
     public int MaxAgeInYears { get; set; }
     public string Frequency { get; set; }
+    public string Content { get; set; }
 }
---
+++ raisin-pets.Data/Migrations/20230307154403_AddContentToTutorials.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace raisinpets.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddContentToTutorials : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Content",
+                table: "Tutorials",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Content",
+                table: "Tutorials");
+        }
+    }
+}
